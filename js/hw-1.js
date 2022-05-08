3dfb6c71d077df609946c03c38eb91719ec1a8f9
AChekov <4ekoff@gmail.com>
2022-05-08 11:23:17 +0300

    Update hw-1.js

---
+++ js/hw-1.js
@@ -0,0 +1,4 @@
+// Просим клиента подтвердить бронь на отель
+// и сохраняем в переменную результат работы confirm
+const isComing = prompt("Please confirm hotel reservation");
+console.log(isComing);
