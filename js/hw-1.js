aaf38f73523c1c8168b41142b721a4454f8065b2
AChekov <4ekoff@gmail.com>
2022-05-08 13:23:32 +0300

    Additional session

--- js/hw-1.js
+++ js/hw-1.js
@@ -1,4 +1,45 @@
-// Просим клиента подтвердить бронь на отель
-// и сохраняем в переменную результат работы confirm
-const isComing = prompt("Please confirm hotel reservation");
-console.log(isComing);
+// function greeting() {
+//   sayHello();
+//   console.log();
+// }
+
+// function sayHello() {
+//   greeting();
+// }
+
+// greeting();
+
+const inputRef = document.querySelector("input");
+const btnStart = document.getElementById("start");
+const btnClick = document.getElementById("click");
+const btnCurrent = document.getElementById("current");
+const btnTotal = document.getElementById("total");
+let count = 0;
+
+btnClick.setAttribute("disabled", "disabled");
+
+btnStart.addEventListener("click", onStart);
+btnClick.addEventListener("click", onClick);
+btnCurrent.addEventListener("click", onCurrent);
+
+function onCurrent() {
+  const currentPoint = JSON.parse(sessionStorage.getItem("Current Point"));
+  console.log(currentPoint);
+}
+
+function onClick() {
+  count += 1;
+}
+
+function onStart() {
+  btnClick.removeAttribute("disabled");
+  count = 0;
+  setTimeout(() => {
+    btnClick.setAttribute("disabled", "disabled");
+    sessionStorage.setItem("Current Point", JSON.stringify(count));
+    const bestScore = JSON.parse(localStorage.getItem("Best score"));
+    if (count >= bestScore) {
+      localStorage.setItem("bestScore", JSON.stringify(count));
+    }
+  }, 5000);
+}
